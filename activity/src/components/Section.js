e218afec4f56cadae4b2faf2818ecd4320bbb51f
danielperr <daniel.perez079@gmail.com>
2020-07-20 18:53:10 +0300

    Code formatting

--- activity/src/components/Section.js
+++ activity/src/components/Section.js
@@ -15,123 +15,121 @@ import { ElementMathInput } from './ElementMathInput';
 
 
 const useStyles = makeStyles((theme) => ({
-    sectionPaper: {
-        margin: theme.spacing(2),
-        paddingLeft: theme.spacing(6),
-        paddingRight: theme.spacing(6),
-        paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(2),
-    },
+  sectionPaper: {
+    margin: theme.spacing(2),
+    paddingLeft: theme.spacing(6),
+    paddingRight: theme.spacing(6),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
 }));
 
 
-export function Section (props) {
-    const classes = useStyles();
-    const [answers, setAnswers] = React.useState({});
+export function Section(props) {
+  const classes = useStyles();
+  const [answers, setAnswers] = React.useState({});
 
+  let checkablesAmount = 0;
+  let correctQuestions = new Set();
 
-    let checkablesAmount = 0;
-    let correctQuestions = new Set();
-        
+  const handleAnswer = (questionId, answer) => {
+    answers[questionId] = answer;
+    props.onAnswer(answers);
+  };
 
-    const handleAnswer = (questionId, answer) => {
-        answers[questionId] = answer;
-        props.onAnswer(answers);
-    };
+  const elements = [];
+  props.elements.forEach((element) => {
+    let obj;
+    const answer = props.answers[element.id] || "";
+    switch (element.type) {
+      case 'label':
+        obj = <ElementLabel text={element.text}
+          id={element.id}
+          key={element.id} />;
+        break;
 
-    const elements = [];
-    props.elements.forEach((element) => {
-        let obj;
-        const answer = props.answers[element.id] || "";
-        switch (element.type) {
-            case 'label':
-                obj = <ElementLabel text={element.text}
-                                    id={element.id}
-                                    key={element.id} />;
-                break;
-            case 'image':
-                obj = <ElementImage src={element.src}
-                                    id={element.id}
-                                    key={element.id} />;
-                break;
-            case 'youtube':
-                obj = <ElementYoutube youtubeId={element.youtubeId}
-                                        id={element.id}
-                                        key={element.id} />;
-                break;
-            case 'single-line-text-input':
-                obj = <ElementSingleLineTextInput text={element.text}
-                                                    answer={answer}
-                                                    onAnswer={handleAnswer}
-                                                    id={element.id}
-                                                    key={element.id} />;
-                break;
-            case 'multi-line-text-input':
-                    obj = <ElementMultiLineTextInput text={element.text}
-                                                        answer={answer}
-                                                        onAnswer={handleAnswer}
-                                                        id={element.id}
-                                                        key={element.id} />;
-                    break;
-            case 'multi-choice':
-                checkablesAmount++;
-                
-                const questionId = element.id;
-                if (answer !== "") {
-                    answers[questionId] = answer;
-                }
+      case 'image':
+        obj = <ElementImage src={element.src}
+          id={element.id}
+          key={element.id} />;
+        break;
 
-                if (questionId in answers) {
-                    const correctIds = element.correct.map(answer => {
-                        return answer.id;
-                    });
+      case 'youtube':
+        obj = <ElementYoutube youtubeId={element.youtubeId}
+          id={element.id}
+          key={element.id} />;
+        break;
 
-                    if (correctIds.includes(answer)) {
-                        correctQuestions.add(questionId);
-                    }
-                }   
+      case 'single-line-text-input':
+        obj = <ElementSingleLineTextInput text={element.text}
+          answer={answer}
+          onAnswer={handleAnswer}
+          id={element.id}
+          key={element.id} />;
+        break;
 
+      case 'multi-line-text-input':
+        obj = <ElementMultiLineTextInput text={element.text}
+          answer={answer}
+          onAnswer={handleAnswer}
+          id={element.id}
+          key={element.id} />;
+        break;
 
-                obj = <ElementMultiChoice text={element.text}
-                                            correct={element.correct}
-                                            incorrect={element.incorrect}
-                                            answer={answer}
-                                            onAnswer={handleAnswer}
-                                            id={element.id}
-                                            key={element.id} />;
-                break;
-            // case 'math-input':
-            //     obj = <ElementMathInput text={element.text}
-            //                             value={element.value}
-            //                             onAnswer={this.handleAnswer}
-            //                             id={element.id}
-            //                             key={element.id} />
-            //     break;
-            default:
-                obj = <label>{"אלמנט לא מזוהה"}</label>;
+      case 'multi-choice':
+        checkablesAmount++;
+
+        const questionId = element.id;
+        if (answer !== "") {
+          answers[questionId] = answer;
         }
-        elements.push(<div className="element"
-                            id={element.id}
-                            key={element.id + "-D"}>
-                                {obj}
-                                <br />
-                        </div>);
-    });
 
-    return (<Paper elevation={5} className={classes.sectionPaper}>
-                <SectionHeader text={props.header}
-                                name={props.id + "-H"} />
-                <div className="section-elements"
-                        key={props.id + "-D"}>
-                            {elements}
-                </div>
-                {checkablesAmount > 0 &&
-                    <button>
-                        בדוק תשובות עבור מקטע זה שאם אתה טועה אכלתה קאשה
+        if (questionId in answers) {
+          const correctIds = element.correct.map(answer => {
+            return answer.id;
+          });
+
+          if (correctIds.includes(answer)) {
+            correctQuestions.add(questionId);
+          }
+        }
+
+        obj = <ElementMultiChoice
+          text={element.text}
+          correct={element.correct}
+          incorrect={element.incorrect}
+          answer={answer}
+          onAnswer={handleAnswer}
+          id={element.id}
+          key={element.id}
+        />;
+        break;
+
+      default:
+        obj = <label>{"אלמנט לא מזוהה"}</label>;
+    }
+    elements.push(<div className="element"
+      id={element.id}
+      key={element.id + "-D"}>
+      {obj}
+      <br />
+    </div>);
+  });
+
+  return (<Paper elevation={5} className={classes.sectionPaper}>
+    <SectionHeader text={props.header}
+      name={props.id + "-H"} />
+    <div className="section-elements"
+      key={props.id + "-D"}>
+      {elements}
+    </div>
+    {checkablesAmount > 0 &&
+      <button>
+        בדוק תשובות עבור מקטע זה שאם אתה טועה אכלתה קאשה
                     </button>
-                }
-                {checkablesAmount === correctQuestions.size &&
-                    <h2> Good Job </h2>
-                }
-            </Paper>);
+    }
+    {checkablesAmount === correctQuestions.size &&
+      <h2> Good Job </h2>
+    }
+  </Paper>);
 }
