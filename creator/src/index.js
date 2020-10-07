be9183a1000e59c199d85fd0a5abdf48e4c925b6
danielperr <daniel.perez079@gmail.com>
2020-10-07 13:54:54 +0300

    Remove activity preset

--- creator/src/index.js
+++ creator/src/index.js
@@ -9,44 +9,9 @@ https://github.com/1ZouLTReX1/iFigures
 ${process.env.NODE_ENV} environment
 `)
 
-const initialOverride = {
-  "mainHeader": "פעילות ריקה",
-  "sections": [
-    {
-      "header": "יחידה ריקה",
-      "elements": [
-        {
-          "type": "multi-choice",
-          "id": "wfqQwgnT0P",
-          "correct": [
-            "x5p9g74IoB"
-          ],
-          "options": [
-            {
-              "text": "תשובה ראשונה",
-              "id": "x5p9g74IoB"
-            },
-            {
-              "text": "תשובה שנייה",
-              "id": "Wb6cbed1lF"
-            },
-            {
-              "text": "תשובה שלישית",
-              "id": "JY2zjo8QBa"
-            }
-          ],
-          "text": "שאלה"
-        }
-      ],
-      "id": "s7IkILYh4f"
-    }
-  ],
-  "id": "cQnmiv3DAT5gy6QC1xSB"
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <App initial={initialOverride} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
