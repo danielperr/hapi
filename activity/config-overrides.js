97bc35d6e5ac15eb7648fdee14e88585e34c74c0
danielperr <daniel.perez079@gmail.com>
2020-11-22 14:28:05 +0200

    Config CRA to building to a single html file

---
+++ activity/config-overrides.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+module.exports = function override(config, env) {
+  config.plugins
+    .find(plugin => Object.getPrototypeOf(plugin).constructor.name === 'HtmlWebpackPlugin')
+    .options.inlineSource = '.(js|css)$'
+  config.plugins.push(new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin));
+
+  return config;
+}
