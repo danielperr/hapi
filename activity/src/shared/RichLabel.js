4c1eac8758ca3c01b9d4ca02a44d32dd605115e8
danielperr <daniel.perez079@gmail.com>
2020-11-30 14:22:45 +0200

    Add latex auto rendering in rich labels

--- activity/src/shared/RichLabel.js
+++ activity/src/shared/RichLabel.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-import ReactMarkdown from 'react-markdown';
+// import ReactMarkdown from 'react-markdown';
+import Markdown from 'markdown-to-jsx';
 import { withStyles } from "@material-ui/core";
+import renderMathInElement from 'katex/dist/contrib/auto-render';
 
 const CustomCss = withStyles({
   '@global': {
@@ -12,21 +14,42 @@ const CustomCss = withStyles({
     'img': {
       maxWidth: '50%',
     },
+    '.katex': {
+      direction: 'ltr',
+      flip: false,
+      marginRight: '4px',
+      marginLeft: '4px',
+    },
+    '.katex-html': {
+      display: 'inline-flex',
+      flexDirection: 'row',
+    }
   },
 })(() => null);
 
 function RichLabel(props) {
+  const labelRef = useRef(null);
+
+  useEffect(() => {
+    if (labelRef.current) {
+      renderMathInElement(labelRef.current, {
+        delimiters: [
+          { left: '$$', right: '$$', display: true },
+          { left: '$', right: '$', display: false },
+        ],
+      });
+    }
+  }, [labelRef]);
+
   return (
     <React.Fragment>
       <CustomCss />
       <label
         className={props.className}
         htmlFor={props.htmlFor}
-        // dangerouslySetInnerHTML={{
-        //   __html: md.render(props.children),
-        // }}
+        ref={labelRef}
       >
-        <ReactMarkdown
+        {/* <ReactMarkdown
           source={props.children}
           renderers={{link: props =>
             <a
@@ -36,7 +59,10 @@ function RichLabel(props) {
             >
               {props.children}
             </a>}}
-        />
+        /> */}
+        <Markdown>
+          {props.children}
+        </Markdown>
       </label>
     </React.Fragment>
   );
