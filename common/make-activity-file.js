559a8a071d09292b8f93b745fe6639e5702b776a
danielperr <daniel.perez079@gmail.com>
2020-12-16 12:57:32 +0200

    Replace tabs with spaces

--- common/make-activity-file.js
+++ common/make-activity-file.js
@@ -1,42 +1,42 @@
 const CONTAINER_TEMPLATE = (structure, answers, filename, activityUrl) => `
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>${filename}</title>
-		<style>
-			#container {
-  			position: fixed;
-  			top: 0;
-  			left: 0;
-  			bottom: 0;
-  			right: 0;
-  			width: 100%;
-  			height: 100%;
-  			border: none;
-  			margin: 0;
-  			padding: 0;
-  			overflow: hidden;
-  			z-index: 999999;
-			}
-		</style>
-	</head>
-	<body>
-		<iframe
-			src="${activityUrl}"
-			id="container"
-		></iframe>
-		<script type="text/javascript">
-			var container = document.getElementById('container');
-			container.onload = function() {
-				container.contentWindow.postMessage({
-					message: 'getContents', value: {
-  					structure: ${structure},
-  			   	answers: ${answers},
-					},
-				},'*');
-			};
-		</script>
-	</body>
+  <head>
+    <title>${filename}</title>
+    <style>
+      #container {
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        border: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        z-index: 999999;
+      }
+    </style>
+  </head>
+  <body>
+    <iframe
+      src="${activityUrl}"
+      id="container"
+    ></iframe>
+    <script type="text/javascript">
+      var container = document.getElementById('container');
+      container.onload = function() {
+        container.contentWindow.postMessage({
+          message: 'getContents', value: {
+            structure: ${structure},
+            answers: ${answers},
+          },
+        },'*');
+      };
+    </script>
+  </body>
 </html>
 `;
 
@@ -49,11 +49,11 @@ const CONTAINER_TEMPLATE = (structure, answers, filename, activityUrl) => `
  * @param {string} activityUrl URL of the activity file online
  */
 function makeActivityContainer(structure, answers, filename, activityUrl) {
-	return CONTAINER_TEMPLATE(
-		JSON.stringify(structure),
-		JSON.stringify(answers || {}),
-		filename,
-		activityUrl,
-	);
+  return CONTAINER_TEMPLATE(
+    JSON.stringify(structure),
+    JSON.stringify(answers || {}),
+    filename,
+    activityUrl,
+  );
 }
 
