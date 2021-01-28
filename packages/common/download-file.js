b101dfdf4e825e7b0ed32de6e9ee13101d69ddba
danielperr <daniel.perez079@gmail.com>
2021-01-28 02:04:47 +0200

    🔧 Fix a bug where the creator wouldn't download files

--- packages/common/download-file.js
+++ packages/common/download-file.js
@@ -8,7 +8,7 @@ export default function downloadFile(filename, contents) {
   const element = document.createElement('a');
   element.setAttribute(
     'href',
-    `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`,
+    `data:text/plain;charset=utf-8,${encodeURIComponent(contents)}`,
   );
   element.setAttribute('download', filename);
   element.style.display = 'none';
