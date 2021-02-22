706100691e900447675e430528056aeff95721e2
danielperr <daniel.perez079@gmail.com>
2021-02-22 14:06:44 +0200

    ➕ Add prop type fmt and styling in component docs

--- packages/hapi-docs/src/components/ComponentDoc/index.jsx
+++ packages/hapi-docs/src/components/ComponentDoc/index.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
+import styles from './styles.module.css';
+
+function getPropTypeClassName(type) {
+  switch (type) {
+    case 'number':
+      return styles.propTypeNumber;
+    case 'bool':
+      return styles.propTypeBoolean;
+    case 'string':
+      return styles.propTypeString;
+    default:
+      return styles.propTypeOther;
+  }
+}
+
+function getNodeFromPropType(type) {
+  switch (type.name) {
+    case 'custom':
+      return type.raw;
+    case 'number':
+    case 'bool':
+    case 'string':
+    case 'func':
+    case 'node':
+    case 'object':
+      return <span className={getPropTypeClassName(type.name)}>{type.name}</span>;
+    default:
+      return (
+        <>
+          <span className={styles.propTypeOther}>{type.name}: </span>
+          {JSON.stringify(type.value)}
+        </>
+      );
+  }
+}
+
 function ComponentDoc({ docs }) {
   return (
     <div>
@@ -7,21 +43,23 @@ function ComponentDoc({ docs }) {
       <p>{docs.description}</p>
       <h2>Props</h2>
       <table>
-        <thead>
+        <tbody>
           <tr>
             <th>Name</th>
-            <th>Description</th>
-            <th>Is Required</th>
             <th>Type</th>
+            <th>Default Value</th>
+            <th>Description</th>
           </tr>
-        </thead>
-        <tbody>
-          {Object.entries(docs.props || {}).map(([name, { description, required, type }], index) => (
+          {Object.entries(docs.props || {}).map(([propName, prop], index) => (
             <tr key={index}>
-              <td>{name}</td>
-              <td>{description}</td>
-              <td>{required ? 'Yes' : 'No'}</td>
-              <td>{JSON.stringify(type)}</td>
+              <td>{propName}</td>
+              <td>{getNodeFromPropType(prop.type)}</td>
+              <td>
+                {prop.required
+                  ? <span className={styles.propRequired}>Required</span>
+                  : <span className={getPropTypeClassName(prop.type.name)}>{prop.defaultValue?.value}</span>}
+              </td>
+              <td>{prop.description}</td>
             </tr>
           ))}
         </tbody>
